411136120f87890438df1f83fadf95ac921a1dff
claytonoliver <claytonfra299@gmail.com>
2023-11-18 19:31:12 -0300

    css adicionado

--- jogoteca.py
+++ jogoteca.py
@@ -1,27 +1,31 @@
-from flask import Flask, render_template, request
-class jogo:
+from flask import Flask, render_template, request, redirect
+class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
 
+jogo1 = Jogo('Tetris', 'puzzle', 'atari')
+jogo2 = Jogo('csGo', 'Tiro', 'pc')
+jogo3 = Jogo('Read dead Redenption', 'Ação', 'Todos')
+lista = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
 
-@app.route('/inicio')
-def ola():
-
-    jogo1 = jogo('Tetris', 'puzzle', 'atari')
-    jogo2 = jogo('csGo', 'Tiro', 'pc')
-    jogo3 = jogo('Read dead Redenption', 'Ação', 'Todos')
-
-
-    lista = [jogo1, jogo2, jogo3]
+@app.route('/')
+def index():
     return render_template('lista.html', titulo= 'jogos', jogos =lista)
-
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo = 'novo jogo')
+@app.route('/criar', methods = ['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
 
-app.run()
+app.run(debug=True)
